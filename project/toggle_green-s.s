7ddd2cf785220521cc4c42f8ce4986a25b9f22a2
Christian <cavasquez1771@gmail.com>
2021-07-15 14:20:44 -0600

    updated makefile to have to seperate loads for c and assembly. bugfix in toggle_green-s.s

--- project/toggle_green-s.s
+++ project/toggle_green-s.s
@@ -8,11 +8,11 @@
 	.extern green_on
 	.extern led_changed
 	.extern led_update
-	.global toggle_green()
+	.global toggle_green
 
 	.data
 	
 toggle_green:
-	xor 1, green_on
-	mov 1, led_changed
+	xor #1, &green_on
+	mov #1, &led_changed
 	call led_update
